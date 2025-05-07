922aae49ebc50d802bb84c0507c1e81a9534e17c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-07 23:15:36 +0600

    Solved second code problem of quiz

--- quiz.c
+++ quiz.c
@@ -1,18 +1,37 @@
+// #include <stdio.h>
+
+// void swap(int *p, int *q)
+// {
+//     int temp = *p;
+//     *p = *q;
+//     *q = temp;
+// }
+
+// int main()
+// {
+//     int a = 6, b = 5;
+//     swap(&a, &b);
+
+//     printf("%d %d", a, b);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-void swap(int *p, int *q)
+void m(int *p)
 {
-    int temp = *p;
-    *p = *q;
-    *q = temp;
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d", p[i]);
+    }
 }
 
 int main()
 {
-    int a = 6, b = 5;
-    swap(&a, &b);
-
-    printf("%d %d", a, b);
+    int a[5] = {6, 5, 3};
+    m(a);
 
     return 0;
 }
